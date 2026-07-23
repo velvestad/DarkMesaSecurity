3cebdc49a7b78dd406390f2cd1cb534402bd8061
velvestad <47135697+velvestad@users.noreply.github.com>
2026-07-23 12:46:10 +0200

    #25: Hopefully fixed state backup importing using scp. Added the existing state of external vSwitch to configuration to stop it from being replaced.

--- infrastructure/terraform/.test/external/networking.tf
+++ infrastructure/terraform/.test/external/networking.tf
@@ -4,6 +4,13 @@ resource "hyperv_network_switch" "classroom_sw" {
   notes                                   = "Virtual switch for external network connectivity (internet)"
   switch_type                             = "External"
   net_adapter_names                       = ["Ethernet"] # This name is not tested
+  id                                      = "CLASSROOM-SW"
+  
+  # Hyper-V default switch settings
+  default_flow_minimum_bandwidth_absolute = 100000000
+  default_queue_vmmq_enabled              = true
+  default_queue_vrss_enabled              = true
+  minimum_bandwidth_mode                  = "Absolute"
 }
 
 resource "hyperv_network_switch" "wan_sw" {
