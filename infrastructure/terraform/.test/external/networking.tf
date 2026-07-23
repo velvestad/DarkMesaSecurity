aea87aadd524c31b5a4aadc1d6bde76fddeb3620
velvestad <47135697+velvestad@users.noreply.github.com>
2026-07-23 14:10:03 +0200

    #25: Trying with a comma.

--- infrastructure/terraform/.test/external/networking.tf
+++ infrastructure/terraform/.test/external/networking.tf
@@ -3,7 +3,9 @@ resource "hyperv_network_switch" "classroom_sw" {
   name                                    = "CLASSROOM-SW"
   notes                                   = "Virtual switch for external network connectivity (internet)"
   switch_type                             = "External"
-# net_adapter_names                       = ["Ethernet"] # Single name inside the array does not work.
+  net_adapter_names                       = [
+    "Ethernet",
+    ] # Single name inside the array does not work.
   
   # Hyper-V default switch settings
   default_flow_minimum_bandwidth_absolute = 100000000
